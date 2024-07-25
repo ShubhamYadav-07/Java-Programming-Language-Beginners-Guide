0b0b728e9c2ae4c9f7ffb6723af9ec0f1b41d1ad
Shubham Yadav <yadav07072005@gmail.com>
2024-07-25 20:16:46 +0530

    SumOfTwoNum

---
+++ SumOfTwoNum.java
@@ -0,0 +1,9 @@
+public class SumOfTwoNum{
+    public static void main(String args[]){
+        int a = 10;
+        int b = 5;
+        int sum;
+        sum = a + b ;
+        System.out.println(sum);
+    }
+}
