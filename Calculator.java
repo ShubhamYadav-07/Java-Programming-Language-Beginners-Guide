bca40e8c53f9b6f194a0001730adba4765fd0962
Shubham Yadav <yadav07072005@gmail.com>
2024-07-26 21:00:25 +0530

    Optimized CalculatorUsingSwitch

--- Calculator.java
+++ Calculator.java
@@ -2,8 +2,11 @@ import java.util.*;
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num1 :");
         int num1 = sc.nextInt();
+        System.out.println("Enter num2 :");
         int num2 = sc.nextInt();
+        System.out.println("Enter operator +,-,*,/ or % : ");
         char operator = sc.next().charAt(0);
         switch (operator){
             case '+' : System.out.println(num1+num2);
