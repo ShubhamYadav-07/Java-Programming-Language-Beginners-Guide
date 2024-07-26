e728d6149e01b88e2f5809b025bb576bf7697f34
Shubham Yadav <yadav07072005@gmail.com>
2024-07-26 17:29:07 +0530

    LargestOfThreeNum

---
+++ LargestOfThreeNum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class LargestOfThreeNum {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int num3=sc.nextInt();
+        if((num1>=num2) && (num1>=num3)){
+            System.out.println(num1+" is greatest.");
+        }
+        else if(num2>=num3){
+            System.out.println(num2+" is greatest.");
+        }
+        else{
+            System.out.println(num3+" is greatest.");
+        }
+    }
+}
