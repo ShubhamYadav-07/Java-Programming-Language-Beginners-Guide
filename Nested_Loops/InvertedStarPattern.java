006c024e312ca4c34f05d6e64833a0e4f68db8d7
Shubham Yadav <yadav07072005@gmail.com>
2024-07-30 11:00:54 +0530

    Inverted Star Pattern

---
+++ Nested_Loops/InvertedStarPattern.java
@@ -0,0 +1,17 @@
+package Nested_Loops;
+
+import java.util.*;
+
+public class InvertedStarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int line = sc.nextInt();
+        for(int i=1;i<=line;i++){
+            for(int j=1;j<=(4-i+1);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }    
+}
